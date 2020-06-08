3d6113d8d6e8ea3eefa124395605cc6d9e1f7cd6
Sidnei Simeão <sidneisimmon@gmail.com>
2020-06-08 14:49:53 -0300

    Finalizado testes

--- index.js
+++ index.js
@@ -4,23 +4,43 @@ const server = express();
 
 server.use(express.json());
 
+const ckeckUserExists = (req, res, next) => {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+  return next();
+};
+
+const checkUserInArray = (req, res, next) => {
+  const { index } = req.params;
+  const user = users[index];
+  if (!user) {
+    return res.status(400).json({ error: "User not found" });
+  }
+  req.user = user;
+  return next();
+};
+
+server.use((req, res, next) => {
+  console.log(`Método: ${req.method}; URL: ${req.url}`);
+  return next();
+});
+
 const users = ["Diego", "Cláudio", "Vitor"];
 
 server.get("/users", (req, res) => res.json(users));
 
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params;
-
-  return res.json(users[index]);
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
-server.post("/users/", (req, res) => {
+server.post("/users/", ckeckUserExists, (req, res) => {
   const { name } = req.body;
   users.push(name);
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserInArray, ckeckUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
   users[index] = name;
